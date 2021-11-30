85dc37dbc4b3ed950f565ec929bf3349732bea4b
EgorShiryaev <shiryaevvyatsu@gmail.com>
2021-11-30 14:20:43 +0300

    create project

---
+++ lib/firebase_config.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+class DefaultFirebaseConfig {
+  static get platformOptions {
+    if (kIsWeb) {
+      return const FirebaseOptions(
+        apiKey: "AIzaSyAChfoaIozVx3GWdJ4WVZxuMq_WfMIs0NU",
+        authDomain: "cinema-control-system.firebaseapp.com",
+        // databaseURL: ,
+        projectId: "cinema-control-system",
+        storageBucket: "cinema-control-system.appspot.com",
+        messagingSenderId: "764973555459",
+        appId: "1:764973555459:web:3af96231e3ff5a57d39336",
+        // measurementId: 
+      );
+    } 
+    // else if (Platform.isIOS || Platform.isMacOS) {
+    //   return const FirebaseOptions(
+    //     apiKey: apiKey,
+    //     appId: appId,
+    //     messagingSenderId: messagingSenderId,
+    //     projectId: projectId,
+    //   );
+    // } else {
+    //   return const FirebaseOptions(
+    //     apiKey: apiKey,
+    //     appId: appId,
+    //     messagingSenderId: messagingSenderId,
+    //     projectId: projectId,
+    //   );
+    // }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:admin_website/firebase_config.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'admin_website.dart';
+
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
+
+  runApp(AdminWebsite());
+}
