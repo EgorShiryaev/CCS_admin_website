c39ff8dd692040d335da2f366782b2169ca04ccd
EgorShiryaev <shiryaevvyatsu@gmail.com>
2021-12-19 21:22:35 +0300

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'admin_website.dart';
 
-Future<void> main(List<String> args) async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: FirebaseConfig.platformOptions);
   runApp(const AdminWebsite());
